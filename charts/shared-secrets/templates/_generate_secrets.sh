1840231a734dba10ed806a916da6bbd36e475254
Ian Baum <ibaum@gitlab.com>
2018-08-07 10:12:06 -0500

    Make workhorse secret key configurable

--- charts/shared-secrets/templates/_generate_secrets.sh
+++ charts/shared-secrets/templates/_generate_secrets.sh
@@ -81,4 +81,4 @@ cp /etc/ssh/ssh_host_* host_keys/
 generate_secret_if_needed {{ template "gitlab.gitlab-shell.hostKeys.secret" . }} --from-file host_keys
 
 # Gitlab-workhorse secret
-generate_secret_if_needed {{ template "gitlab.gitlab-workhorse.secret" . }} --from-literal=shared_secret=$(gen_random 'a-zA-Z0-9' 32 | base64)
+generate_secret_if_needed {{ template "gitlab.workhorse.secret" . }} --from-literal={{ template "gitlab.workhorse.key" . }}=$(gen_random 'a-zA-Z0-9' 32 | base64)
--- templates/_workhorse.tpl
+++ templates/_workhorse.tpl
@@ -3,8 +3,11 @@
 {{/*
 Return the gitlab-workhorse secret
 */}}
-{{- define "gitlab.gitlab-workhorse.secret" -}}
+
+{{- define "gitlab.workhorse.secret" -}}
 {{- default (printf "%s-gitlab-workhorse-secret" .Release.Name) .Values.global.workhorse.secret | quote -}}
 {{- end -}}
 
-
+{{- define "gitlab.workhorse.key" -}}
+{{- default "shared_secret" .Values.global.workhorse.key | quote -}}
+{{- end -}}
